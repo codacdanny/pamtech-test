d897b7631dd58266e905e6288825657ee1db0fd3
Chinemerem Daniel <danpresh302@gmail.com>
2024-02-29 12:46:08 +0100

    built the nav section

--- src/Major Components/Nav/index.tsx
+++ src/Major Components/Nav/index.tsx
@@ -1,4 +1,5 @@
-import { Box, Flex, Image } from "@chakra-ui/react";
+import { Box, Button, Flex, Image, Text } from "@chakra-ui/react";
+import { AiOutlineDown } from "react-icons/ai";
 import { colors } from "../../Variables/variables";
 import logo from "../../assets/logo.svg";
 import home from "../../assets/home.svg";
@@ -6,17 +7,43 @@ import calender from "../../assets/calendar.svg";
 import services from "../../assets/category-2.svg";
 import customers from "../../assets/category-3.svg";
 import NavButton from "../../Minor Components/NavButton";
+
 const Nav = () => {
   return (
-    <Box height="100px" backgroundColor={colors.primaryColor}>
-      <Flex>
-        <Image src={logo} />
+    <Box height="100px" backgroundColor={colors.primaryColor} paddingY="1rem">
+      <Flex alignItems="center">
+        <Box>
+          <Image src={logo} alt="logo" />
+        </Box>
         <Flex>
           <NavButton imageUrl={home} title="Dashboard" href="#" />
           <NavButton imageUrl={calender} title="Bookings" href="#" />
           <NavButton imageUrl={customers} title="Customers" href="#" />
           <NavButton imageUrl={services} title="Services" href="#" />
         </Flex>
+        <Box>
+          <Button
+            variant="ghost"
+            colorScheme="transparent"
+            outline="2px solid #eee"
+            iconSpacing={5}
+            rightIcon={
+              <AiOutlineDown
+                size="12px"
+                fontWeight={700}
+                color={colors.headerColor}
+              />
+            }>
+            <Flex flexDirection="column" textAlign="left">
+              <Text fontSize=".9rem" color={colors.headerColor}>
+                Buukmenow Demo
+              </Text>
+              <Text fontSize=".7rem" color={colors.textColor}>
+                Buukmenow@gmail.com{" "}
+              </Text>
+            </Flex>
+          </Button>
+        </Box>
       </Flex>
     </Box>
   );
